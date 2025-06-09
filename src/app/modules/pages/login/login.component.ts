cdbaca4c36e238154ad95890f73522e83316ff8b
Higor S <stanleyhigor1@gmail.com>
2025-06-09 17:41:02 -0300

    create added

--- src/app/modules/pages/login/login.component.ts
+++ src/app/modules/pages/login/login.component.ts
@@ -20,6 +20,7 @@ export class LoginComponent {
   });
 
   toggleCreate = false;
+  toggleForgot = false;
 
   createAccountForm = new FormGroup({
     email: new FormControl<string>('', [Validators.required, Validators.email]),
@@ -29,6 +30,7 @@ export class LoginComponent {
     cnpj: new FormControl<string>('', [Validators.pattern('^[0-9]{14}$')]),
     nome: new FormControl<string>('', [Validators.required, Validators.minLength(3)]),
     telefone: new FormControl<string>('', [Validators.required, Validators.pattern('^[0-9]{10,11}$')]),
+    dataNascimento: new FormControl<string>('', [Validators.required]),
   });
 
   constructor() { }
@@ -45,6 +47,10 @@ export class LoginComponent {
         alert('O email teste@teste ja utiliza a senha 123456');
         this.loginForm.reset();
     }
+  }
+
+  onCreateAccount() {
+    console.log('ok');
   }
 
 }
